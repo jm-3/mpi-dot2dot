b204cae50dd696c0d5e1c8124952752ade6a61f4
José M. Martín <jmartin@onsager.ugr.es>
2020-08-11 17:31:37 +0200

    output file is removed if correct. A empty file is created to indicate that the output was ok

--- scripts/slurm/checkResults.sh
+++ scripts/slurm/checkResults.sh
@@ -13,6 +13,14 @@ ORIGFILE="$HOME/TFM/resultados/$DATASET/Dot-1.0.p3/threads-1/test-1/output.dot"
 
 for i in `ls -1 ~/TFM/resultados/$DATASET/$ALGNAME/`; do
         for j in `ls -1 ~/TFM/resultados/$DATASET/$ALGNAME/$i`; do
-                diff -q $ORIGFILE ~/TFM/resultados/$DATASET/$ALGNAME/$i/$j/$k/output.dot
+                CHECKFILE="$HOME/TFM/resultados/$DATASET/$ALGNAME/$i/$j/$k/output.dot"
+                OKFILE="$HOME/TFM/resultados/$DATASET/$ALGNAME/$i/$j/$k/ok-output.dot"
+                if [ ! -f $OKFILE ]; then
+                        diff -q $ORIGFILE $CHECKFILE
+                        if [ "$?" -eq "0" ]; then
+                                touch $OKFILE
+                                rm -f $CHECKFILE
+                        fi
+                fi
         done
 done
