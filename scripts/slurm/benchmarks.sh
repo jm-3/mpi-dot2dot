ed02c351f66efd59a9e4a36c84dfd75575b00c16
José M. Martín <jmartin@onsager.ugr.es>
2020-08-13 18:20:16 +0200

    algorithm name added to jobname

--- scripts/slurm/benchmarks.sh
+++ scripts/slurm/benchmarks.sh
@@ -121,7 +121,7 @@ for i in "${data[@]}"; do
                 outname="${i}"
                 executable_baseopts="-s $datasetsDir/$i -c $configfile"
                 executable_opts="$PARAMS ${executable_baseopts} $DOTTHREADS"
-                slanzarv --jobname dot-d$ID-$NUMTHREADS-$j $MPIRUN -c $NUMTHREADS $TIME ./dot $executable_opts -o $OUTPUTFILE
+                slanzarv --jobname $algname-d$ID-$NUMTHREADS-$j $MPIRUN -c $NUMTHREADS $TIME ./dot $executable_opts -o $OUTPUTFILE
         done
 done
 
